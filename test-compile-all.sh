3e76c863fe3bd1711b41a1754ec595563df2a19f
Tuomas Kulve <tuomas@kulve.fi>
2019-01-04 17:42:09 +0200

    test-compile-all.sh: Add mandatory env variables

--- test-compile-all.sh
+++ test-compile-all.sh
@@ -5,6 +5,10 @@ set -ex
 PROJECTS=("test native_posix"
 		  "test nucleo_f411re")
 
+export PATH=$(pwd)/zephyr/build/kconfig:$PATH
+export ZEPHYR_TOOLCHAIN_VARIANT=gccarmemb
+export GCCARMEMB_TOOLCHAIN_PATH=/usr
+
 for project in "${PROJECTS[@]}"
 do
 	IFS=' ' read -r -a params <<< "${project}"
