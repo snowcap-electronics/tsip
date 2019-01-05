b3daf3619931a58b8494da9b1740c44f5958f962
Tuomas Kulve <tuomas@kulve.fi>
2019-01-05 14:59:54 +0200

    test-compile-all.sh: Fix indentation

--- test-compile-all.sh
+++ test-compile-all.sh
@@ -3,7 +3,7 @@
 set -ex
 
 PROJECTS=("test native_posix"
-		  "test nucleo_f411re")
+          "test nucleo_f411re")
 
 export PATH=$(pwd)/zephyr/build/kconfig:$PATH
 export ZEPHYR_TOOLCHAIN_VARIANT=gccarmemb
@@ -11,15 +11,15 @@ export GNUARMEMB_TOOLCHAIN_PATH=/usr
 
 for project in "${PROJECTS[@]}"
 do
-	IFS=' ' read -r -a params <<< "${project}"
-	app="${params[0]}"
-	board="${params[1]}"
-	. tsip-env.sh "${app}" "${board}"
-	rm -rf build
-	mkdir build
-	cd build
-	cmake ..
-	make
-	make clean
-	cd ..
+    IFS=' ' read -r -a params <<< "${project}"
+    app="${params[0]}"
+    board="${params[1]}"
+    . tsip-env.sh "${app}" "${board}"
+    rm -rf build
+    mkdir build
+    cd build
+    cmake ..
+    make
+    make clean
+    cd ..
 done
